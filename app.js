3569d58b3c7930408a9fc3a2297c85b9165663ca
Wisbell <westleyisbell@gmail.com>
2017-01-16 14:21:39 -0600

    Added highway description for the views

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 console.log("app.js loaded")
 
 
-var app = angular.module('', ['ngRoute'])
+var app = angular.module('RouteApp', ['ngRoute'])
 
 
 
@@ -12,7 +12,7 @@ app.config(function($routeProvider){
     //     controller: "",
     //     templateUrl: ""
     // })
-    .when('/blueridge/', {
+    .when('/blueridge', {
         controller: "BlueRidgeParkwayCtrl",
         templateUrl: "partials/blueridgeparkway.html"
     })
@@ -33,12 +33,16 @@ app.config(function($routeProvider){
 
 
 app.controller('BlueRidgeParkwayCtrl', function($scope){
-    $scope.highwayName
-    $scope.highwayDescription
+    $scope.highwayName = "Blue Ridge Parkway"
+    $scope.highwayDescription = `The Blue Ridge Parkway is a National Parkway and All-American Road in the United States,
+                                 noted for its scenic beauty. The parkway, which is America's longest linear park,[3]
+                                  runs for 469 miles (755 km) through 29 Virginia and North Carolina counties`
 })
 
 
 app.controller('Route66Ctrl', function($scope){
-    $scope.highwayName
-    $scope.highwayDescription
+    $scope.highwayName = "Route 66"
+    $scope.highwayDescription = `U.S. Route 66 (US 66 or Route 66), also known as the Will Rogers Highway, the Main Street of America or the Mother Road,
+                                 was one of the original highways within the U.S. Highway System. US 66 was established on November 11, 1926, with road
+                                  signs erected the following year.[4] The highway, which became one of the most famous roads in America`
 })
